c5bc74475846c1d1b8c66910b9a327e680b72abb
Yamamura-Riku <32561860+Yamamura-Riku@users.noreply.github.com>
2017-10-16 10:17:55 +0900

    インポート
    自分の作業していたフォルダをインポート

---
+++ プロジェクト/Project42/Assets/Yamamura/Script/MainCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+    public GameObject playerSmall;  //プレイヤースモール
+    public GameObject playerBig;    //プレイヤービッグ
+    PlayerController smallC;        //プレイヤースモールのスクリプト
+  
+	// Use this for initialization
+	void Start () {
+        smallC = playerSmall.GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (smallC.playerMode == PlayerMode.PLAYER)
+        {
+            transform.position = new Vector3(playerSmall.transform.position.x, playerSmall.transform.position.y, -10);
+        }
+        else if (smallC.playerMode == PlayerMode.NONE)
+        {
+            transform.position = new Vector3(playerBig.transform.position.x, playerBig.transform.position.y, -10);
+        }
+    }
+}
