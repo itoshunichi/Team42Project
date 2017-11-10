87471d516710b928b19890b3cbc6a40dbc7600d5
Yamamura-Riku <32561860+Yamamura-Riku@users.noreply.github.com>
2017-11-10 10:55:42 +0900

    ゲージ
    レベルによって画像が変わるように変更

--- プロジェクト/Project42/Assets/Yamamura/Script/EnergyUI.cs
+++ プロジェクト/Project42/Assets/Yamamura/Script/EnergyUI.cs
@@ -1,22 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EnergyUI : MonoBehaviour {
 
     public GameObject hammer;
     public Energy energy;
-    public GameObject player;
-    
+    Image image;
+    public Sprite level1;
+    public Sprite level2;
 
 	// Use this for initialization
 	void Start () {
-		
+        image = GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        FillAmout();
+        Move();
+	}
+
+    //円ゲージ
+    private void FillAmout()
+    {
+        if (energy.level == 1)
+        {
+            image.sprite = level1;
+            image.fillAmount = energy.GetEnergy() / energy.levelUpEnergy;
+        }
+        else if (energy.level == 2)
+        {
+            image.sprite = level2;
+            image.fillAmount = (energy.GetEnergy() - energy.levelUpEnergy) / energy.levelUpEnergy;
+        }
+    }
+
+    //移動処理
+    private void Move()
+    {
         gameObject.transform.position = hammer.transform.position;
         gameObject.transform.rotation = hammer.transform.rotation;
-	}
+    }
 }
--- プロジェクト/Project42/Assets/Yamamura/Script/PlayerSmallController.cs
+++ プロジェクト/Project42/Assets/Yamamura/Script/PlayerSmallController.cs
@@ -30,6 +30,7 @@ public class PlayerSmallController : Player
         }
     }
 
+    //向きに対して移動
     private void RotationMove()
     {
         if (transform.rotation.z != flick.transform.rotation.z) GetComponent<Rigidbody2D>().velocity = Vector2.zero;
@@ -49,6 +50,7 @@ public class PlayerSmallController : Player
         if (col.gameObject.tag == "BeDestroyedObject")
         {
             isHit = true;
+            speed = 0;
             Debug.Log("HIT");
             Vector2 dir = transform.position - col.gameObject.transform.position;
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
