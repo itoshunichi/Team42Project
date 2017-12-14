79d9ce6dd12ea7d9bb5d9de57095d48597bc3ef6
Yamamura-Riku <32561860+Yamamura-Riku@users.noreply.github.com>
2017-12-14 23:07:01 +0900

    ゲームオーバー処理追加

--- プロジェクト/Project42/Project42/Assets/Yamamura/Script/Object/Player/PlayerSmallController.cs
+++ プロジェクト/Project42/Project42/Assets/Yamamura/Script/Object/Player/PlayerSmallController.cs
@@ -6,12 +6,13 @@ public class PlayerSmallController : Player
 {
     float accelerator = 0;                  //加速
     public float acceleratorMax;            //加速の最大値
-    private bool isMove;                //止まっているかどうか
+    private bool isMove;                    //止まっているかどうか
     public Energy energy;                   //エネルギー
     private int enemyHitCount = 0;          //エネミーに当たった回数
     public GameObject[] point;
     public GameObject[] effect;
     private Vector2 spriteSize;
+
     public int EnemyHitCount 
     { get { return enemyHitCount; } }
     public bool IsMove
@@ -39,9 +40,18 @@ public class PlayerSmallController : Player
     // Update is called once per frame
     void Update()
     {
-        Move();//移動処理
-        Accelerator();//加速処理
-        GetComponent<Animator>().SetBool("IsHit", isHit);
+        if(energy.GetEnergy() <= 0)
+        {
+            Instantiate(effect[1], transform.position, effect[1].transform.rotation);
+            Destroy(gameObject);
+            Destroy(GameObject.Find("HammerFront"));
+        }
+        else if (energy.GetEnergy() > 0)
+        {
+            Move();//移動処理
+            Accelerator();//加速処理
+            GetComponent<Animator>().SetBool("IsHit", isHit);
+        }
     }
 
     private void Move()
@@ -118,7 +128,7 @@ public class PlayerSmallController : Player
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
             isHit = true;
             speed = 0;
-            Instantiate(effect[0], transform.position, effect[1].transform.rotation);
+            Instantiate(effect[1], transform.position, effect[1].transform.rotation);
             Vector2 dir = transform.position - col.gameObject.transform.position;
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             GetComponent<Rigidbody2D>().AddForce(dir * collisionPower);
