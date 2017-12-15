68d8a423939788977edcc489ee82a458224ee2a7
Yamamura-Riku <32561860+Yamamura-Riku@users.noreply.github.com>
2017-12-15 11:19:11 +0900

    統合でやった処理

---
+++ プロジェクト/Project42/Project42/Assets/Yamamura/Script/TouchEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEffect : MonoBehaviour {
+
+    public GameObject touchEffect;
+    
+    private void Awake() 
+    {
+        //シーンを跨いでも消えないように
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var touch = Instantiate(touchEffect, Input.mousePosition, touchEffect.transform.rotation);
+            touch.transform.parent = transform;
+        }
+	}
+}
