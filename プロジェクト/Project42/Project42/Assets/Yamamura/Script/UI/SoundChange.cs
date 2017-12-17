24384db93bbf24cebddf4970a50ad24d423241e2
Yamamura-Riku <32561860+Yamamura-Riku@users.noreply.github.com>
2017-12-18 08:58:14 +0900

    TITle
    SE調整ボタンを押したときSEがなるようにした

--- プロジェクト/Project42/Project42/Assets/Yamamura/Script/UI/SoundChange.cs
+++ プロジェクト/Project42/Project42/Assets/Yamamura/Script/UI/SoundChange.cs
@@ -19,28 +19,38 @@ public class SoundChange : MonoBehaviour
         Mathf.Clamp(bgmImage.fillAmount, 0.0f, 1.0f);
         Mathf.Clamp(seImage.fillAmount, 0.0f, 1.0f);
     }
-
+    /// <summary>
+    /// BGM音量上げる
+    /// </summary>
     public void BGMPlus()
     {
         bgmImage.fillAmount += 0.2f;
         AudioManager.Instance.ChangeBGMVolume(bgmImage.fillAmount);
     }
-
+    /// <summary>
+    /// SE音量上げる
+    /// </summary>
     public void SEPlus()
     {
         seImage.fillAmount += 0.2f;
         AudioManager.Instance.ChangeSEVolume(seImage.fillAmount);
+        AudioManager.Instance.PlaySE(AUDIO.SE_DECISION);
     }
-
+    /// <summary>
+    /// BGM音量下げる
+    /// </summary>
     public void BGMMinus()
     {
         bgmImage.fillAmount -= 0.2f;
         AudioManager.Instance.ChangeBGMVolume(bgmImage.fillAmount);
     }
-
+    /// <summary>
+    /// SE音量下げる
+    /// </summary>
     public void SEMinus()
     {
         seImage.fillAmount -= 0.2f;
         AudioManager.Instance.ChangeSEVolume(seImage.fillAmount);
+        AudioManager.Instance.PlaySE(AUDIO.SE_DECISION);
     }
 }
